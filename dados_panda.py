a2401e7bf8a421c3baaf47bcba652aa6173fcf21
Carlos Lima <caddu.lima@hotmail.com>
2025-08-31 01:01:09 -0300

    Update dados_panda.py

--- dados_panda.py
+++ dados_panda.py
@@ -1,1 +1,13 @@
+#codigo para fazer a leitura do dataframe
 
+# Print the head of the homelessness data
+print(homelessness.head())
+
+# Print information about homelessness
+print(homelessness.info())
+
+# Print the shape of homelessness
+print(homelessness.shape)
+
+# Print a description of homelessness
+print(homelessness.describe())
